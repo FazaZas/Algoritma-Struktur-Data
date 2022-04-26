3b4ce619f1f799383cd6adf1b411d655fbb4b36b
FazaZas <fazanur17@gmail.com>
2022-04-26 11:04:26 +0700

    M11

--- M11-Sorting 2 (bubble & shell)/tugas/bubble-sort.c
+++ M11-Sorting 2 (bubble & shell)/tugas/bubble-sort.c
@@ -1,36 +1,81 @@
 #include <stdio.h>
-void print(int a[], int n)
+#include <stdlib.h>
+#include <time.h>
+#define MAX 10
+
+time_t t1, t2;
+long waktuKomputasi;
+
+void printArray(int data[])
 {
-     int i;
-     for (i = 0; i < n; i++)
+     for (int i = 0; i < MAX; i++)
      {
-          printf("%d ", a[i]);
+          printf("%d ", data[i]);
      }
+     puts("");
 }
-void bubble(int a[], int n)
+
+void randArray(int data[])
 {
-     int i, j, temp;
-     for (i = 0; i < n; i++)
+     for (int i = 0; i < MAX; i++)
      {
-          for (j = i + 1; j < n; j++)
+          data[i] = rand() / 3000 + 1;
+          printf("%d ", data[i]);
+     }
+     puts("");
+}
+
+void bubbleSort(int data[])
+{
+     int did_swap = 1;
+     int i, j;
+     int temp;
+     int banding = 0, tukar = 0, geser = 0;
+
+     for (i = 0; i < MAX; i++)
+     {
+          if (did_swap == 1)
           {
-               if (a[j] < a[i])
+               did_swap = 0;
+               for (j = 0; j < MAX - i - 1; j++)
                {
-                    temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
+                    banding = banding + 1;
+                    ;
+                    if (data[j] > data[j + 1])
+                    {
+                         tukar = tukar + 1;
+                         temp = data[j];
+                         data[j] = data[j + 1];
+                         data[j + 1] = temp;
+                         geser = geser + 3;
+                         did_swap = 1;
+                    }
                }
           }
      }
+     printArray(data);
+     puts("");
+     printf("Jumlah Perbandingan %d \n", banding);
+     printf("Jumlah Pertukaran %d \n", tukar);
+     printf("Jumlah Pergeseran %d \n", geser);
 }
-void main()
+
+int main()
 {
-     int i, j, temp;
-     int a[5] = {10, 35, 32, 13, 26};
-     int n = sizeof(a) / sizeof(a[0]);
-     printf("sebelum di sorting - \n");
-     print(a, n);
-     bubble(a, n);
-     printf("\nsetelah bubble sorting - \n");
-     print(a, n);
+     int nilai[MAX];
+     // int nilai[MAX] = {3, 5, 1, 9, 2, 0, 7, 8, 6, 4};
+     printf("--- BUBBLE SORT ---\n");
+     puts("");
+     printf("Sebelum Diurutkan : ");
+     randArray(nilai);
+     // printArray(nilai);
+     printf("Setelah Diurutkan : ");
+     time(&t1);
+     bubbleSort(nilai);
+     time(&t2);
+     waktuKomputasi = t2 - t1;
+
+     puts("");
+     printf("Waktu Komputasi = %g\n", waktuKomputasi);
+     return 0;
 }
