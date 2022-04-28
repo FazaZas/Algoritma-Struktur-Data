623a3a53f511e0fe0aebfd8bece48910e1eed6e8
FazaZas <fazanur17@gmail.com>
2022-04-28 10:55:54 +0700

    m11

--- M11-Sorting 2 (bubble & shell)/tugas/shell-sort.c
+++ M11-Sorting 2 (bubble & shell)/tugas/shell-sort.c
@@ -1,38 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 10
 
-int shell(int a[], int n)
+time_t t1, t2;
+long waktuKomputasi;
+
+void printArray(int data[])
 {
-     /* Atur ulang elemen array pada n/2, n/4, ..., 1 interval */
-     for (int interval = n / 2; interval > 0; interval /= 2)
+     for (int i = 0; i < MAX; i++)
      {
-          for (int i = interval; i < n; i += 1)
-          {
-               /* simpan a[i] ke variabel temp dan kosongkan posisi ke-i */
-               int temp = a[i];
-               int j;
-               for (j = i; j >= interval && a[j - interval] > temp; j -= interval)
-                    a[j] = a[j - interval];
+          printf("%d ", data[i]);
+     }
+     printf("\n");
+}
 
-               // letakkan temp (asli a[i]) di posisi yang benar
-               a[j] = temp;
+void shellSort(int data[])
+{
+     int did_swap = 1;
+     int i, jarak;
+     int temp;
+     int banding = 0, tukar = 0, geser = 0;
+
+     jarak = MAX;
+     sleep(3);
+     while (jarak > 1)
+     {
+          jarak /= 2;
+          did_swap = 1;
+          while (did_swap == 1)
+          {
+               did_swap = 0;
+               i = 0;
+               while (i < (MAX - jarak))
+               {
+                    banding++;
+                    if (data[i] > data[i + jarak])
+                    {
+                         tukar++;
+                         temp = data[i];
+                         data[i] = data[i + jarak];
+                         data[i + jarak] = temp;
+                         did_swap = 1;
+                    }
+                    i++;
+               }
           }
      }
-     return 0;
+
+     printArray(data);
+     puts("");
+     printf("Jumlah Perbandingan %d \n", banding);
+     printf("Jumlah Pertukaran %d \n", tukar);
 }
-void printArr(int a[], int n)
-{
-     int i;
-     for (i = 0; i < n; i++)
-          printf("%d ", a[i]);
-}
+
 int main()
 {
-     int a[] = {33, 31, 40, 8, 12, 17, 25, 42};
-     int n = sizeof(a) / sizeof(a[0]);
-     printf("sebelum sorting - \n");
-     printArr(a, n);
-     shell(a, n);
-     printf("\nsetelah shell sorting - \n");
-     printArr(a, n);
+     int nilai[MAX] = {3, 5, 1, 9, 2, 0, 7, 8, 6, 4};
+     printf("--- SHELL SORT ---\n");
+     puts("");
+     printf("Sebelum Diurutkan : ");
+     printArray(nilai);
+     printf("Setelah Diurutkan : ");
+     time(&t1);
+     shellSort(nilai);
+     time(&t2);
+     waktuKomputasi = t2 - t1;
+     // printArray(nilai);
+
+     puts("");
+     printf("Waktu Komputasi = %g\n", waktuKomputasi);
      return 0;
 }
